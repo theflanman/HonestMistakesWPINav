2850860ded00f19a21d9037c93d561e5ba010c87
Lauren <Lauren@Lauren-Vaio2>
2015-11-11 17:27:50 -0500

    initial dev tool commit

--- HonestMistakesWPINav/src/MapNode.java
+++ HonestMistakesWPINav/src/MapNode.java
@@ -1,9 +1,3 @@
-/** TODO write this
- * 
- * @author Connor Flanigan
- *
- */
-
 public class MapNode {
 	
 	private float xPos;
@@ -15,8 +9,9 @@ public class MapNode {
 	private float gScore;
 	private float hScore;
 	private MapNode cameFrom;
-	private GlobalMap globalMap;
-	private LocalMap localMap;
+//	private GlobalMap globalMap;
+//	private LocalMap localMap;
+	private String nodeName;
 	//private Attributes attributes
 	
 	public float calcDistance(MapNode toNode) {
@@ -28,5 +23,43 @@ public class MapNode {
 	public void addNeighbor(MapNode node) {
 		
 	}
-
+	
+	public MapNode(int x, int y, int count) {
+		xPos = (float) x;
+		yPos = (float) y;
+		nodeID = count;
+	}
+	
+	public float getxPos() {
+		return xPos;
+	}
+	
+	public float getyPos() {
+		return yPos;
+	}
+	public float getzPos() {
+		return zPos;
+	}
+	public String getnodeName() {
+		return nodeName;
+	}
+	public void setxPos(float pos) {
+		xPos = pos;
+	}
+	public void setyPos(float pos) {
+		yPos = pos;
+	}
+	public void setnodeName(String name) {
+		nodeName = name;
+	}
+	public int getID(){
+		return nodeID;
+	}
+	public String getNeighborsString() {
+		String result = "";
+		for(int i=0; i < neighbors.length; i++) {
+			result += neighbors[i].getnodeName();
+		}
+		return result;
+	}
 }
