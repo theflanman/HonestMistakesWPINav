948eebede4724c42244e1041a7ee30e57f23bca3
ngigliotti <ngigliotti@wpi.edu>
2015-12-06 15:41:28 -0500

    Finished imprioving email GUI, added random colors just for now to be changed by the themes later

--- src/main/gui/EmailGUI.java
+++ src/main/gui/EmailGUI.java
@@ -16,7 +16,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.ArrayList;
 
 import javax.swing.JTextArea;
 
@@ -55,6 +54,7 @@ public class EmailGUI extends JDialog {
 	}
 
 	public EmailGUI() {
+		getContentPane().setBackground(new Color(95, 172, 213));
 		setUndecorated(true);
 		setType(Type.UTILITY);
 		setBounds(100, 100, 684, 359);
@@ -72,28 +72,10 @@ public class EmailGUI extends JDialog {
 			}
 		});
 		
-		// Send Button
-		JButton btnSend = new JButton("Send");
-		btnSend.setBounds(496, 314, 79, 29);
-		getContentPane().add(btnSend);
-		
-		// When the send button is pressed:
-		// the test is the text fields is saved to the email contents
-		// the email is sent
-		// the window is no longer shown
-		btnSend.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("Emailing Directions");
-				subject = txtSubject.getText();
-				body = body.concat(directions);
-				String[] toEmail = txtTo.getText().split(";");
-				email.sendFromGMail(toEmail, subject, body);
-				EmailGUI.this.setVisible(false);
-			}
-		});
-		
 		// To address text field
 		txtTo = new JTextField();
+		txtTo.setBackground(Color.WHITE);
+		txtTo.setForeground(Color.GRAY);
 		txtTo.setText("Type an email followed by a \";\" to add multiple recipents");
 		txtTo.setBounds(120, 71, 530, 29);
 		getContentPane().add(txtTo);
@@ -106,6 +88,7 @@ public class EmailGUI extends JDialog {
 			  public void mouseClicked(MouseEvent e) {
 				  if (toAreaIndex == 0) {
 					  txtTo.setText("");
+					  txtTo.setForeground(Color.BLACK);
 					  toAreaIndex ++;
 				  }
 			  }
@@ -113,50 +96,37 @@ public class EmailGUI extends JDialog {
 		
 		// Subject text field
 		txtSubject = new JTextField();
+		txtSubject.setBackground(Color.WHITE);
+		txtSubject.setForeground(Color.BLACK);
 		txtSubject.setText(subject);
 		txtSubject.setColumns(10);
 		txtSubject.setBounds(120, 129, 530, 29);
 		getContentPane().add(txtSubject);
 		
-		
-		// Body/Custom Message text area
-		JTextArea txtBody = new JTextArea();
-		txtBody.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		txtBody.setText(body);
-		txtBody.setLineWrap(true);
-		txtBody.setBounds(120, 186, 530, 108);
-		getContentPane().add(txtBody);
-		
-		// Waiting for a mouse press to clear the default text the first time
-		// TODO Make the default text grey and then changed to black text
-		txtBody.addMouseListener(new MouseAdapter() {
-			  @Override
-			  public void mouseClicked(MouseEvent e) {
-				  if (bodyAreaIndex == 0) {
-					  txtBody.setText("");
-					  bodyAreaIndex ++;
-				  }
-			  }
-			});
-		
 		// To label
 		JLabel lblTo = new JLabel("To:");
-		lblTo.setBounds(72, 75, 32, 20);
+		lblTo.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblTo.setForeground(Color.WHITE);
+		lblTo.setBounds(79, 74, 32, 20);
 		getContentPane().add(lblTo);
 		
 		// Subject label
 		JLabel lblSubject = new JLabel("Subject:");
-		lblSubject.setBounds(41, 133, 58, 20);
+		lblSubject.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblSubject.setForeground(Color.WHITE);
+		lblSubject.setBounds(41, 133, 70, 20);
 		getContentPane().add(lblSubject);
 		
 		// Message label
 		JLabel lblMessage = new JLabel("Message:");
-		lblMessage.setBounds(36, 186, 69, 20);
+		lblMessage.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblMessage.setForeground(Color.WHITE);
+		lblMessage.setBounds(36, 186, 79, 20);
 		getContentPane().add(lblMessage);
 		
 		// Top Bar panel
 		JPanel topPanel = new JPanel();
-		topPanel.setBackground(new Color(65, 105, 225));
+		topPanel.setBackground(new Color(218, 211, 203));
 		topPanel.setForeground(Color.WHITE);
 		topPanel.setBounds(0, 0, 684, 38);
 		getContentPane().add(topPanel);
@@ -165,8 +135,59 @@ public class EmailGUI extends JDialog {
 		// Top bar text
 		JLabel lblNewLabel = new JLabel("Email Directions");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
-		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setForeground(Color.BLACK);
 		lblNewLabel.setBounds(15, 0, 138, 36);
 		topPanel.add(lblNewLabel);
+		
+		// Body panel
+		JPanel bodyPanel = new JPanel();
+		bodyPanel.setBackground(new Color(95, 172, 213));
+		bodyPanel.setBounds(121, 189, 529, 98);
+		getContentPane().add(bodyPanel);
+		bodyPanel.setLayout(null);
+		
+		
+		// Body/Custom Message text area
+		JTextArea txtBody = new JTextArea();
+		txtBody.setBackground(Color.WHITE);
+		txtBody.setForeground(Color.GRAY);
+		txtBody.setBounds(0, 0, 533, 93);
+		bodyPanel.add(txtBody);
+		txtBody.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtBody.setText(body);
+		txtBody.setLineWrap(true);
+		
+		// Waiting for a mouse press to clear the default text the first time
+		// TODO Make the default text grey and then changed to black text
+		txtBody.addMouseListener(new MouseAdapter() {
+			  @Override
+			  public void mouseClicked(MouseEvent e) {
+				  if (bodyAreaIndex == 0) {
+					  txtBody.setText("");
+					  txtTo.setForeground(Color.BLACK);
+					  bodyAreaIndex ++;
+				  }
+			  }
+			});
+		
+		// Send Button
+				JButton btnSend = new JButton("Send");
+				btnSend.setBounds(496, 314, 79, 29);
+				getContentPane().add(btnSend);
+				
+				// When the send button is pressed:
+				// the test is the text fields is saved to the email contents
+				// the email is sent
+				// the window is no longer shown
+				btnSend.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						subject = txtSubject.getText();
+						body = txtBody.getText();
+						body = body.concat("\n").concat(directions);
+						String[] toEmail = txtTo.getText().split(";");
+						email.sendFromGMail(toEmail, subject, body);
+						EmailGUI.this.setVisible(false);
+					}
+				});
 	}
 }
